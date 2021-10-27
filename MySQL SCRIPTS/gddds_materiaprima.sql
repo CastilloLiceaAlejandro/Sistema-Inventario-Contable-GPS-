03a0505f7d6163c99e34ea7c544b0bb63d195450
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2021-10-26 17:39:30 -0700

    Add files via upload

---
+++ MySQL SCRIPTS/gddds_materiaprima.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gddds
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `materiaprima`
+--
+
+DROP TABLE IF EXISTS `materiaprima`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materiaprima` (
+  `idMATERIAPRIMA` varchar(10) NOT NULL,
+  `descrMATERIAPRIMA` varchar(50) NOT NULL,
+  `cprovMATERIAPRIMA` varchar(10) NOT NULL,
+  `nombrePROVEEDORES` varchar(45) NOT NULL,
+  `registroMATERIAPRIMA` date NOT NULL,
+  `cantidadMATERIAPRIMA` int NOT NULL,
+  PRIMARY KEY (`idMATERIAPRIMA`),
+  UNIQUE KEY `idCOMPRAS_UNIQUE` (`idMATERIAPRIMA`),
+  UNIQUE KEY `descrPRODUCTOS_UNIQUE` (`descrMATERIAPRIMA`),
+  KEY `nombrePROVEEDORES_idx` (`nombrePROVEEDORES`),
+  CONSTRAINT `nombrePROVEEDORES` FOREIGN KEY (`nombrePROVEEDORES`) REFERENCES `proveedores` (`nombrePROVEEDORES`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materiaprima`
+--
+
+LOCK TABLES `materiaprima` WRITE;
+/*!40000 ALTER TABLE `materiaprima` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materiaprima` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-26 17:35:07
