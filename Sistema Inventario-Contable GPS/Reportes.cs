f4cd0d79a6038654b341e1b55175a08aaa6051c2
CastilloLiceaAlejandro <alejandro.castillo17@tectijuana.edu.mx>
2021-11-22 16:47:13 -0800

    .

---
+++ Sistema Inventario-Contable GPS/Reportes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Inventario_Contable_GPS
+{
+    class ReporteAlmacen
+    {
+        public int idEntrada { get; set; }
+        public string fechaEntrada { get; set; }
+        public int idEmpleado { get; set; }
+    }
+    class Reportes
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string edad { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+        public string contraseña { get; set; }
+        public string turno { get; set; }
+        public int id_puesto { get; set; }
+
+    }
+
+}
